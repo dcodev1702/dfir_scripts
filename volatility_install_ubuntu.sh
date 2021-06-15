27a5d3b47f697dfe9e8f2d9693d65d9be5739b60
Dev_Bits_1702 <32214072+DevBits1702@users.noreply.github.com>
2021-06-15 16:27:29 -0400

    A bash script to install volatility on Ubuntu Linux.
    A basic bash script to install volatility and it's various dependencies on Ubuntu Linux
    
    Referenced:  https://github.com/volatilityfoundation/volatility/wiki/Installation#installing-volatility
    Python 2.7 (Dev)
    Pip2
      -- Distorm3 (3.4.4)
      -- pytz
      -- ujson
      -- pycrypto
      -- yara

---
+++ volatility_install_ubuntu.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/bash
+
+###########################################
+# Author: @2bitsend0xdea
+# Date: 16 JUN 2021
+# Usage: sudo ./volatility_install.sh
+# Req: Written for Ubuntu Linux
+############################################
+
+install_volatility () {
+
+  sudo add-apt-repository universe
+  sudo apt update
+
+  sudo apt install pcregrep libpcre++-dev python2-dev python-setuptools build-essential yara zip git -y
+
+  echo "Time to install PIP for Python2"
+  curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
+
+  sudo python2 get-pip.py
+  sudo rm -f get-pip.py
+
+  echo "Clone the volatility repo"
+  git clone https://github.com/volatilityfoundation/volatility.git
+
+  echo "Installing pip packages required for volatility"
+
+  sudo python2 -m pip install distorm3==3.4.4 pycrypto pytz yara openpyxl ujson
+
+  # This is VERY important, volatility will BREAK under Ubuntu 19 or greater
+  sudo ln -s /usr/local/lib/python2.7/dist-packages/usr/lib/libyara.so /usr/lib/libyara.so
+
+
+  echo "Volatility pre-req's have been met, time to install volatility (/usr/loca/bin)"
+
+  cd volatility
+  sudo python2.7 setup.py install
+
+  cd ../
+  sudo rm -rf volatility
+
+  echo "Thank you for playing 'Install Volatility for 1000', lets rip some memory apart!"
+}
+
+
+main () {
+
+  echo -e "Lets install some volatility and forensicate!"
+
+  if [ $(egrep '^(NAME)=' /etc/os-release | cut -d '=' -f2 | xargs) == "Ubuntu" ]; then
+     echo "We're on Ubuntu, lets install volatility!"
+     install_volatility
+  else
+     echo "We're on something else and I'm not about that life!"
+  fi
+
+}
+
+# Call main() to run the script!
+main
